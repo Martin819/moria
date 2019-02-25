0c44f3cbe8be031acc91df1d99b601093cf9a588
pavel-borik <pavel.borik1@gmail.com>
2019-02-25 19:47:03 +0100

    Moved example response into separate file

---
+++ client/src/utils/exampleResponse.js
@@ -0,0 +1,88 @@
+export const transactionsData = {
+  transactions: [
+    {
+      id: 1,
+      valueDate: '2018-12-25T15:41:50.126Z',
+      transactionType: 'CARD',
+      value: {
+        amount: 220,
+        currency: 'CZK'
+      },
+      partyDescription: 'Billa',
+      category: 'ELECTRONICS',
+      message: 'Additional info'
+    },
+    {
+      id: 2,
+      valueDate: '2019-02-24T15:41:50.126Z',
+      transactionType: 'CARD',
+      value: {
+        amount: 2200,
+        currency: 'EUR'
+      },
+      partyDescription: 'Tesco',
+      category: 'FOOD',
+      message: 'Additional info'
+    },
+    {
+      id: 3,
+      valueDate: '2019-01-27T15:41:50.126Z',
+      transactionType: 'CARD',
+      value: {
+        amount: 250,
+        currency: 'USD'
+      },
+      partyDescription: 'Albert',
+      category: 'HOME EQUIPMENT',
+      message: 'Additional info'
+    },
+    {
+      id: 4,
+      valueDate: '2019-01-27T15:41:50.126Z',
+      transactionType: 'CARD',
+      value: {
+        amount: 1500,
+        currency: 'CZK'
+      },
+      partyDescription: 'Albert',
+      category: 'HOME EQUIPMENT',
+      message: 'Additional info'
+    },
+    {
+      id: 5,
+      valueDate: '2019-01-27T15:41:50.126Z',
+      transactionType: 'CARD',
+      value: {
+        amount: 888,
+        currency: 'CZK'
+      },
+      partyDescription: 'Albert',
+      category: 'HOME EQUIPMENT',
+      message: 'Additional info'
+    },
+    {
+      id: 6,
+      valueDate: '2019-01-25T15:41:50.126Z',
+      transactionType: 'CARD',
+      value: {
+        amount: 777,
+        currency: 'CZK'
+      },
+      partyDescription: 'Albert',
+      category: 'HOME EQUIPMENT',
+      message: 'Additional info'
+    },
+    {
+      id: 7,
+      valueDate: '2019-01-25T15:41:50.126Z',
+      transactionType: 'CARD',
+      value: {
+        amount: 333,
+        currency: 'CZK'
+      },
+      partyDescription: 'Albert',
+      category: 'HOME EQUIPMENT',
+      message: 'Additional info'
+    }
+  ]
+};
