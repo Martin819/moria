2e88cae880cca7c2d5b3fad728d9ad6b0f32accc
OstruszkaAdam <adam.ostruszka@seznam.cz>
2019-02-26 21:36:18 +0100

    Implementace knihovny Lombok

--- server/server/src/main/java/moria/server/MockData/Item.java
+++ server/server/src/main/java/moria/server/MockData/Item.java
@@ -1,23 +1,11 @@
 package moria.server.MockData;
 
+import lombok.*;
+
+@Data
 public class Item {
 
     private String name;
     private int count;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getCount() {
-        return count;
-    }
-
-    public void setCount(int count) {
-        this.count = count;
-    }
 }
