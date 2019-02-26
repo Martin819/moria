35ef093031ca6619100c3152edf3efde1defe005
vladabraha <vladislav.braha@gmail.com>
2019-02-26 19:36:26 +0100

    add mediatype

--- server/server/src/main/java/moria/server/controller/MainController.java
+++ server/server/src/main/java/moria/server/controller/MainController.java
@@ -1,13 +1,15 @@
 package moria.server.controller;
 
 import moria.server.MockData.Item;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 
-    @RequestMapping("/items")
+    @GetMapping(path = "/items", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public Item sendItems() {
         Item item = new Item();
         item.setCount(1);
