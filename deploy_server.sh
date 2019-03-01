b4b8f7a378d6c0b833cb7eae5b1b0de4c39a58c2
Martin Polreich <polreichmartin@gmail.com>
2019-03-01 18:29:49 +0100

    Several fixtures for deploy script

--- deploy_server.sh
+++ deploy_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Deploys services on top of deployed k8s cluster
-# Don't forget to aquire your credentials first
+# Don't forget to aquire your credentials first (gcloud container clusters get-credentials <cluster-name> --zone <zone> --project <project>)
 
 # Usage example: bash deploy_server.sh db-cluster <root-pass> userdb <userpass> moria db-cluster-node prometheus-grafana-1 80 3000 grafana-node
 
@@ -22,14 +22,18 @@ tar zxfv helm-v2.6.2-linux-amd64.tar.gz
 cp linux-amd64/helm .
 kubectl create clusterrolebinding user-admin-binding --clusterrole=cluster-admin --user=$(gcloud config get-value account)
 kubectl create serviceaccount tiller --namespace kube-system
+kubectl create clusterrolebinding tiller-admin-binding --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
 kubectl create clusterrolebinding --clusterrole=cluster-admin --serviceaccount=default:default concourse-admin
 ./helm init --service-account=tiller
 ./helm update
+echo "##### Wait for Tiller account to get ready #####"
+sleep 20
 echo "Please check if both server and client are the same version:"
 ./helm version
 
 echo "##### Install XtraDB cluster #####"
-helm install --name ${xtradb_cluster_name} --set mysqlRootPassword=${mysql_root_password},mysqlUser=${mysql_user_name},mysqlPassword=${mysql_user_password},mysqlDatabase=${mysql_database} stable/percona-xtradb-cluster
+./helm install --name ${xtradb_cluster_name} --set mysqlRootPassword=${mysql_root_password},mysqlUser=${mysql_user_name},mysqlPassword=${mysql_user_password},mysqlDatabase=${mysql_database} stable/percona-xtradb-cluster
+sleep 10
 
 echo "##### Expose XtraDB node #####"
 kubectl expose service ${xtradb_cluster_name}-pxc --port=3306 --target-port=3306 --name=${xtradb_node_name} --type="LoadBalancer"
