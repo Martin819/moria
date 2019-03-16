810c33259b32ff8dbb5e332b16c114dc66270ec1
vladabraha <vladislav.braha@gmail.com>
2019-03-16 20:05:57 +0100

    Remove unnecessary code

--- server/src/main/java/moria/utils/CategoryScorer.java
+++ server/src/main/java/moria/utils/CategoryScorer.java
@@ -2,8 +2,6 @@ package moria.utils;
 
 import me.xdrop.fuzzywuzzy.FuzzySearch;
 import moria.SpringContext;
-import moria.dto.Category;
-import moria.dto.Rule;
 import moria.model.rules.Ruleset;
 import moria.model.transactions.Transaction;
 import moria.model.transactions.TransactionPartyAccount;
@@ -13,31 +11,21 @@ import org.joda.time.Duration;
 import org.joda.time.Interval;
 
 import java.math.BigDecimal;
-import java.text.ParseException;
-import java.util.*;
+import java.util.Date;
+import java.util.List;
+import java.util.TreeMap;
 
 public class CategoryScorer {
-
-    private HashMap<String, ArrayList<String>> categories = new HashMap<>();
-    private HashMap<String, Double> scoredCategories = new HashMap<>();
-
-    private HashMap<String, ArrayList<String>> dummyCategories = new HashMap<>();
 
     private List<Ruleset> ruleSet;
     private Transaction transaction;
 
-    //for test only
+    //for test purpose only
     public int category;
 
     public int scoreCategories(Transaction transaction) {
         loadAllRules();
         this.transaction = transaction;
-
-        //sem se budou hodnotit skore pravdepodobnosti, kam to ma spadnout - nejdřív zkopíruju názvy kategorií do HashMapy
-//        for (Map.Entry<String, ArrayList<String>> entry : categories.entrySet()) {
-//            String categoryName = entry.getKey();
-//            scoredCategories.put(categoryName, 0);
-//        }
 
         //sem se budou hodnotit skore pravdepodobnosti, kam to ma spadnout - nejdřív zkopíruju názvy kategorií do HashMapy
         TreeMap<Double, Integer> categories = new TreeMap<>();
@@ -194,32 +182,6 @@ public class CategoryScorer {
 
 
     private void loadAllRules() {
-
-//        //dummy data for Rules
-//        HashMap<String, String> dummyRule = new HashMap<>();
-//        dummyRule.put("payer", "Decathlon");
-//        dummyRule.put("transferMoney", "500");
-//        dummyRule.put("compare", ">");
-//        dummyRule.put("incomingPayment", "false");
-//        Date date = new Date();
-//        dummyRule.put("dateOfPayment", String.valueOf(date));
-//        dummyRule.put("description", "nakup funkcniho obleceni Decathlon");
-//
-//        Rule rule = new Rule("Sport", "věci na běhání", dummyRule);
-//
-//        HashMap<String, String> dummyRule2 = new HashMap<>();
-//        dummyRule2.put("payer", "Shell");
-//        dummyRule2.put("compare", ">");
-//        dummyRule2.put("transferMoney", "1200");
-//        dummyRule2.put("incomingPayment", "false");
-//        dummyRule2.put("dateOfPayment", String.valueOf(date));
-//        dummyRule2.put("description", "shell PowerV");
-//
-//        Rule rule2 = new Rule("Pohonne hmoty", "nakup benzinu do autaku", dummyRule2);
-//
-//        allRules.add(rule);
-//        allRules.add(rule2);
-
         // vytáhnutí pravidel z databáze
         RulesetService rulesetService = getRulesetService();
         ruleSet = rulesetService.findAllRulesets();
@@ -237,24 +199,5 @@ public class CategoryScorer {
             if (category > max) max = category;
         }
         category = max;
-
-//        //dummy data (tohle jakoby budeme mit (preddefinovany pravidla)
-//        ArrayList<String> supermarkets = new ArrayList<>();
-//        supermarkets.add("Billa");
-//        supermarkets.add("Kaufland");
-//        supermarkets.add("Penny Market");
-//        dummyCategories.put("Jidlo", supermarkets);
-//
-//        ArrayList<String> retailers = new ArrayList<>();
-//        retailers.add("C&A");
-//        retailers.add("H&M");
-//        dummyCategories.put("Oblečení", retailers);
-//
-//        categories = dummyCategories;
-//
-//        dummyCategories.put("Nájem", null);
-//        dummyCategories.put("Výplata", null);
-//        dummyCategories.put("Úroky", null);
-//        category.setCategories(dummyCategories);
     }
 }
