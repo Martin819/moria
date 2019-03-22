a2f56f22437cd97914a2e9623dc16f253ed3bfbf
pavel-borik <pavel.borik1@gmail.com>
2019-03-22 15:25:57 +0100

    Added pagination

--- client/src/components/transactions-dashboard/transaction-list/TransactionList.js
+++ client/src/components/transactions-dashboard/transaction-list/TransactionList.js
@@ -1,47 +1,83 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import { Grid } from '@material-ui/core/';
-import TransactionItem from '../transaction-item/TransactionItem';
-import { Spinner } from 'reactstrap';
 import TransactionFilterContainer from '../../../containers/transactions/TransactionFilterContainer';
+import TransactionItem from '../transaction-item/TransactionItem';
+import Pagination from 'react-js-pagination';
+import { withStyles, Grid } from '@material-ui/core';
+import { Spinner } from 'reactstrap';
 
-const TransactionsList = props => {
-  const { classes, transactions, isLoading } = props;
-  const transactionItems = transactions.map((transaction, index) => {
+class TransactionsList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activePage: 1,
+      itemsCountPerPage: 8
+    };
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.transactions.length != prevProps.transactions.length) {
+      this.setState({ activePage: 1 });
+    }
+  }
+
+  handlePageChange = pageNumber => {
+    this.setState({ activePage: pageNumber });
+  };
+
+  render() {
+    const { activePage, itemsCountPerPage } = this.state;
+    const { classes, transactions, isLoading, handleTransactionCategorySubmit } = this.props;
+    const transactionItems = transactions
+      .slice((activePage - 1) * itemsCountPerPage, activePage * itemsCountPerPage)
+      .map((transaction, index) => {
+        return (
+          <TransactionItem
+            key={transaction.id}
+            index={index}
+            {...transaction}
+            handleTransactionCategorySubmit={handleTransactionCategorySubmit}
+          />
+        );
+      });
+
     return (
-      <TransactionItem
-        key={transaction.id}
-        index={index}
-        {...transaction}
-        handleTransactionCategorySubmit={props.handleTransactionCategorySubmit}
-      />
+      <div className={classes.root}>
+        <h2>Transactions</h2>
+        <Grid container direction="column" spacing={8}>
+          <Grid item xs>
+            <TransactionFilterContainer />
+          </Grid>
+          <Grid item xs>
+            {isLoading ? (
+              <div className="text-center">
+                <Spinner type="grow" color="primary" />
+              </div>
+            ) : (
+              transactionItems
+            )}
+          </Grid>
+          <Grid item container xs justify="center" className="mt-4">
+            <Pagination
+              itemClass="page-item"
+              linkClass="page-link"
+              pageRangeDisplayed={5}
+              activePage={activePage}
+              totalItemsCount={transactions.length}
+              itemsCountPerPage={itemsCountPerPage}
+              onChange={this.handlePageChange}
+            />
+          </Grid>
+        </Grid>
+      </div>
     );
-  });
-
-  return (
-    <div className={classes.root}>
-      <h2>Transactions</h2>
-      <Grid container direction="column" spacing={8}>
-        <Grid item xs>
-          <TransactionFilterContainer />
-        </Grid>
-        <Grid item xs>
-          {isLoading ? (
-            <div className="text-center">
-              <Spinner type="grow" color="primary" />
-            </div>
-          ) : (
-            transactionItems
-          )}
-        </Grid>
-      </Grid>
-    </div>
-  );
-};
+  }
+}
 
 TransactionsList.propTypes = {
-  classes: PropTypes.object.isRequired
+  classes: PropTypes.object.isRequired,
+  transactions: PropTypes.array.isRequired,
+  isLoading: PropTypes.bool.isRequired
 };
 
 const styles = theme => ({
