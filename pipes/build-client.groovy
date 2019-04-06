9d44b51f4fbd7fa0bcc8dea7b0a53afa1f127ef6
Martin Polreich <polreichmartin@gmail.com>
2019-04-06 13:20:14 +0200

    Update build-client pipeline to support .env file

--- pipes/build-client.groovy
+++ pipes/build-client.groovy
@@ -6,7 +6,15 @@ node {
       }
       mvnHome = tool 'M3'
    }
-   stage('Build') {
+   stage('Set Backend env') {
+        dir('moria/client') {
+          sh "echo 'REACT_APP_API_HOST=localhost' > .env"
+          sh "echo 'REACT_APP_API_PORT=8083' >> .env"
+          println("Using following backend configuraion: ")
+          sh "cat .env"
+        }
+   }
+   stage('Install NPM') {
       dir('moria/client') {
         nodejs(nodeJSInstallationName: 'NodeJS') {
           sh "npm install --no-optional"
@@ -15,6 +23,13 @@ node {
           sh "npm install typescript@* --no-optional"
           sh "npm install ts-pnp@1.0.1 --no-optional"
           sh "npm install --no-optional"
+          sh "npm run build"
+        }
+      }
+   }
+   stage('Build') {
+      dir('moria/client') {
+        nodejs(nodeJSInstallationName: 'NodeJS') {
           sh "npm run build"
         }
       }
